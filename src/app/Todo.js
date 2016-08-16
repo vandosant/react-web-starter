27bdd92504159cbb6fd65ca503ce925aa50fd3d3
vandosant <sjskender@gmail.com>
2016-08-15 20:56:01 -0600

    Setup todo using setState

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+    }
+  }
+
+  handleChange(e) {
+    const value = e.target.value;
+    this.setState({newTodo: value});
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ''});
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form>
+	  <input onChange={this.handleChange.bind(this)} value={this.state.newTodo} type="text" placeholder="new todo"/>
+	  <button onClick={this.handleClick.bind(this)}>create</button>
+        </form>
+	<ul>
+          {this.state.todos.map(todo => <li>{todo}</li>)}
+	</ul>
+      </div>
     );
   }
 }
